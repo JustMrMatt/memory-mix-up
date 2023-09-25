abaec4c6ee921c012a5c1c414c097afa15b8a0d2
Matt Allen <mallen@trimarkdigital.com>
2023-09-24 20:13:42 -0400

    Updates

---
+++ precache-manifest.d3defb682633254d9e3e228eea2868c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d11689307fda13315a2a7e9116c2252e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61bff7b3875f9a602daf",
+    "url": "/static/css/main.253abfb2.chunk.css"
+  },
+  {
+    "revision": "fdf5c704dc8387299cec",
+    "url": "/static/js/2.7fb3fe6c.chunk.js"
+  },
+  {
+    "revision": "61bff7b3875f9a602daf",
+    "url": "/static/js/main.8f6edfb9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
